b8cbaf85b019fdc3e74b20c4c568734fbf270c3e
Michał <michal.dwojak92@gmail.com>
2017-03-02 00:00:00 +0100

    add - new class code first

---
+++ Miscellaneous technologies/Code_First/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miscellaneous_technologies.Code_First
+{
+    public class Method
+    {
+        MyDbContext _Context = new MyDbContext();
+
+        public List<News> download_list()
+        {
+            return (from p in _Context.News select p).ToList();
+        }
+    }
+}
---
+++ Miscellaneous technologies/Code_First/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Miscellaneous_technologies.Code_First
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+    }
+}
